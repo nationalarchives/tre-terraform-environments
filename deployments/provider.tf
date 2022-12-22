5d0a539be9ced7d64e4f41329268fe739396b8bd
rahmedzaizi <rahmed@zaizi.com>
2022-12-22 09:13:21 +0000

    default_tags added to terraform provider

--- deployments/provider.tf
+++ deployments/provider.tf
@@ -4,6 +4,16 @@ provider "aws" {
     role_arn    = var.assume_role
     external_id = var.external_id
   }
+  default_tags {
+    tags = {
+      Environment     = var.environment_name
+      Owner           = "digital-archiving"
+      Terraform       = true
+      TerraformSource = "https://github.com/nationalarchives/tre-terraform-environments"
+      CostCentre      = "56"
+      Role            = "prvt"
+    }
+  }
 }
 
 terraform {
